0b06797de48e7948ce93d5a614bfc7831c262b95
suzhixiang <suzhixiang1999@gmail.com>
2018-09-04 09:39:56 +0800

    .gitignore

--- DBSCAN/Test1.py
+++ DBSCAN/Test1.py
@@ -1,27 +1,29 @@
 import pandas as pd
 import numpy as np
-input_file="input.xlsx"
-output_file="output.xlsx"
-data=pd.read_excel(input_file)
-r=5
-num=3
-label=1
-vis=np.zeros(len(data))
-
-def dist(a,b):
-    dist1=0
-    for i in range(len(a)):
-        dist1+=np.square((a[i]-b[i]))
-    dist1=np.sqrt(dist1)
-    return dist1
-def DFS(center,label):
+def DBSCAN(input_file,output_file,radius,number):
+    data=pd.read_excel(input_file)
+    label=1
+    vis=np.zeros(len(data))
+    def dist(a,b):
+        dist1=0
+        for i in range(len(a)):
+            dist1+=np.square((a[i]-b[i]))
+        dist1=np.sqrt(dist1)
+        return dist1
+    def DFS(center,label):
+        for i in range(len(data)):
+            if vis[i]==0and(dist(data.values[i],center)<r):
+                vis[i]=label
+                DFS(data.values[i],label)
     for i in range(len(data)):
-        if vis[i]==0and(dist(data.values[i],center)<r):
+        if vis[i]==0:
             vis[i]=label
             DFS(data.values[i],label)
-for i in range(len(data)):
-    if vis[i]==0:
-        vis[i]=label
-        DFS(data.values[i],label)
-        label+=1
-print(vis)
+            label+=1
+    print(vis)
+if __name__ == '__main__':
+    input_file = "input.xlsx"
+    output_file = "output.xlsx"
+    r = 5
+    num = 3
+    DBSCAN(input_file,output_file,r,num)
--- K-Means/Test1.py
+++ K-Means/Test1.py
@@ -1,38 +1,43 @@
 import pandas as pd
 import numpy as np
-input_file="input.xlsx"
-output_file="output.xlsx"
-data=pd.read_excel(input_file)
-initial_center=[
-    [1,1],
-    [10,10],
-    [10,1]
-]
-center=pd.DataFrame(initial_center)
-label=np.zeros(len(data),np.int)
-num=np.zeros(len(center),np.int)
-def dist(x,y):
-    d=0
-    for i in range(len(x)):
-        d+=np.square(x[i]-y[i])
-    d=np.sqrt(d)
-    return d
-for t in range(100):
-    label=np.zeros_like(label,np.int)
-    num = np.zeros_like(num,np.int)
-    for i in range(len(data)):
-        min=1000000000
-        for j in range(len(center)):
-            dist1=dist(data.values[i],center.values[j])
-            if dist1<min:
-                min=dist1
-                label[i]=j
-        num[label[i]]+=1
-    initial_center=np.zeros_like(initial_center,np.float)
+def K_Means(input_file,output_file,initial_center):
+    data=pd.read_excel(input_file)
     center=pd.DataFrame(initial_center)
-    for i in range(len(data)):
-        for j in range(len(center.values[0])):
-            center.values[label[i]][j]+=data.values[i][j]/num[label[i]]
-writer = pd.ExcelWriter(output_file)
-center.to_excel(writer)
-writer.close()
+    label=np.zeros(len(data),np.int)
+    num=np.zeros(len(center),np.int)
+    def dist(x,y):
+        d=0
+        for i in range(len(x)):
+            d+=np.square(x[i]-y[i])
+        d=np.sqrt(d)
+        return d
+    for t in range(100):
+        label=np.zeros_like(label,np.int)
+        num = np.zeros_like(num,np.int)
+        for i in range(len(data)):
+            min=1000000000
+            for j in range(len(center)):
+                dist1=dist(data.values[i],center.values[j])
+                if dist1<min:
+                    min=dist1
+                    label[i]=j
+            num[label[i]]+=1
+        initial_center=np.zeros_like(initial_center,np.float)
+        center=pd.DataFrame(initial_center)
+        for i in range(len(data)):
+            for j in range(len(center.values[0])):
+                center.values[label[i]][j]+=data.values[i][j]/num[label[i]]
+    writer = pd.ExcelWriter(output_file)
+    center.to_excel(writer)
+    writer.close()
+
+if __name__ == '__main__':
+    input_file = "input.xlsx"
+    output_file = "output.xlsx"
+    initial_center = [
+        [1, 1, 1],
+        [10, 10, 10],
+        [10, 1, 10],
+        [10, 100, 100]
+    ]
+    K_Means(input_file, output_file, initial_center)
